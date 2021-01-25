b546e826a6f3af97065749567daaeae6f1efb2d2
tyler belmonte <tylerbelmonte@gmail.com>
2021-01-25 17:32:12 -0600

    1st

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printEqual(10,10,10);
+    }
+
+    public static void printEqual(int one, int two, int three) {
+        if((one < 0) || (two < 0) || (three < 0)) {
+            System.out.println("Invalid Value");
+        } else if ((one == two) && (two == three) && (three == one)) {
+            System.out.println("All numbers are equal");
+        } else if ((one != two) && (two != three) && (three != one)) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
